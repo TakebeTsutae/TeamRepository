fea5732f0647b323b78f6a01a5b49e9505c0d453
Reiji0115 <fko2647019@stu.o-hara.ac.jp>
2026-05-19 18:56:14 +0900

    江上攻撃実装0519

--- ShadowBringer/Assets/AttackController.cs
+++ ShadowBringer/Assets/AttackController.cs
@@ -1,12 +1,55 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AttackController : MonoBehaviour
 {
     public Transform attackPoint;
     public float attackRange = 0.5f;
-    
+    public LayerMask enemyLayer;
+    public int attackDamage = 10;
+    public float attackCooldown = 0.5f;
+
+    private float lastAttackTime = 0f;
+
     void Update()
     {
-        
+        bool canAttack = Time.time >= lastAttackTime + attackCooldown;
+
+
+        // 攻撃ボタンが押され、クールタイムも経過している時だけ攻撃
+        if(Keyboard.current.jKey.isPressed && canAttack)
+        {
+            Attack();
+            lastAttackTime = Time.time;
+        }
+    }
+
+    void Attack()
+    {
+        // 攻撃範囲内の敵をまとめて取得
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(
+            attackPoint.position,
+            attackRange,
+            enemyLayer
+            );
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            var health = enemy.GetComponent<EnemyHealth>();
+            if(health!=null)
+            {
+                health.TakeDamage(attackDamage);
+            }
+        }
+    }
+
+    // エディタ上で攻撃範囲を表示
+    void OndrawGizmosSelected()
+    {
+        if(attackPoint!=null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        }
     }
 }
