76f1fc0b5a6d50d50c7f3fd84c97c879b436e563
hirayamamitsuha <fko2647022@stu.o-hara.ac.jp>
2026-05-24 12:56:48 +0900

    平山0524マップ拡大

--- ShadowBringer/Assets/Scenes/PlayerControl.cs
+++ ShadowBringer/Assets/Scenes/PlayerControl.cs
@@ -3,33 +3,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
-    Rigidbody2D rigid2D;
-    float jumpForce = 300.0f;
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb2d;
+    private Vector2 movement;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Application.targetFrameRate = 60;
-        this.rigid2D = GetComponent<Rigidbody2D>();
+        // Rigidbody2Dのコンポーネントを取得
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.aKey.wasPressedThisFrame)
-        {
-            transform.Translate(-1, 0, 0);
-        }
+        // キーボードの入力を取得 (水平: 左右, 垂直: 上下)
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
 
-        if (Keyboard.current.dKey.wasPressedThisFrame)
-        {
-            transform.Translate(1, 0, 0);
-        }
+        // 移動する方向をVector2に格納
+        movement = new Vector2(moveX, moveY).normalized;
+       }
 
-        if (Keyboard.current.wKey.wasPressedThisFrame)
+
+        void FixedUpdate()
         {
-            this.rigid2D.AddForce(transform.up * this.jumpForce);
+            // 物理演算に基づいた移動処理
+            rb2d.linearVelocity = movement * moveSpeed;
+
+
         }
-    }
 }
     
