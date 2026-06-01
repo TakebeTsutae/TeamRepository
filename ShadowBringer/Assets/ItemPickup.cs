560e8472f7566a6a363eec4df6d7f6dd3127a623
KayabaMao <fko2647004@stu.o-hara.ac.jp>
2026-06-01 18:07:35 +0900

    アイテムのキー操作の追加

--- ShadowBringer/Assets/ItemPickup.cs
+++ ShadowBringer/Assets/ItemPickup.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     GameObject player;
+
+    //追加↓
+    private bool isPlayerNear = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +16,11 @@ public class Item : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 追加↓
+        if(isPlayerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
