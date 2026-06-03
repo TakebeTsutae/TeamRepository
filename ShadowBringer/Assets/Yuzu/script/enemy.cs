87f40e0a50f5c2fc90397cf327bb17829d8cf3cd
MizukamiYuzu <fko2647069@stu.o-hara.ac.jp>
2026-06-03 09:43:21 +0900

    enemy_otamesi

--- ShadowBringer/Assets/Yuzu/script/enemy.cs
+++ ShadowBringer/Assets/Yuzu/script/enemy.cs
@@ -1,60 +1,29 @@
 using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
-    int a;
-    public GameObject Coli;
-    private int _enemyHp = 8;
+    public float moveSpeed = 5f;
     private Rigidbody2D rb;
-    public float speed = 5f;
-    private bool tach = false;
-    float moveHorizontal = -0.01f;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Coli.SetActive(false);
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
     void FixedUpdate()
     {
-        
+        Vector2 velocity = rb.linearVelocity;
+        velocity.x = -moveSpeed;
+        rb.linearVelocity = velocity;
 
-        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
-        rb.MovePosition(rb.position + movement);
-        StartCoroutine(Time());
-        
-        /*float moveHorizontal = -0.01f;
-        
-        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
-
-        rb.MovePosition(rb.position + movement);
-        */
     }
-    private IEnumerator Time()
-    {
-
-
-        moveHorizontal = -0.01f;
-        yield return new WaitForSeconds(3);
-        moveHorizontal = 0;
-        a++;
-        Coli.SetActive(true);
-        yield return new WaitForSeconds(1);
-        print(a);
-        Coli.SetActive(false);
-        moveHorizontal = -0.01f;
-        /*yield return new WaitForSeconds(3);
-        moveHorizontal = 0;*/
-    }
-    
 }
