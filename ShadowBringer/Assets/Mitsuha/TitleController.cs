38436045a3f084d8805c477cf6f998cf0ddcc01f
hirayamamitsuha <fko2647022@stu.o-hara.ac.jp>
2026-06-09 23:02:43 +0900

    タイトルシーン完成

--- ShadowBringer/Assets/Mitsuha/TitleController.cs
+++ ShadowBringer/Assets/Mitsuha/TitleController.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class TitleController : MonoBehaviour
 {
-   
+   public void OnStarButton()
+    {
+        SceneManager.LoadScene("Hirayama1");
+    }
 }
