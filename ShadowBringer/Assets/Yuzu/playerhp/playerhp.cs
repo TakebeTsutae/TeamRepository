854fe34f609bb7e32978a2f7e7d65c7389664cf0
MizukamiYuzu <fko2647069@stu.o-hara.ac.jp>
2026-06-10 22:49:27 +0900

    _playerhp

---
+++ ShadowBringer/Assets/Yuzu/playerhp/playerhp.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class playerhp : MonoBehaviour
+{
+    private int _playerhp=3;
+    public GameObject hp_Max1;
+    public GameObject hp_Max2;
+    public GameObject hp_Max3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        hp_Max1.SetActive(true);
+        hp_Max2.SetActive(true);
+        hp_Max3.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(_playerhp);
+        if (_playerhp < 4)
+        {
+            hp_Max1.SetActive(true);
+            hp_Max2.SetActive(true);
+            hp_Max3.SetActive(true);
+        }
+        if (_playerhp < 3)
+        {
+            hp_Max1.SetActive(true);
+            hp_Max2.SetActive(true);
+            hp_Max3.SetActive(false);
+        }
+        if (_playerhp < 2)
+        {
+            hp_Max1.SetActive(true);
+            hp_Max2.SetActive(false);
+        }
+        if (_playerhp < 1)
+        {
+            hp_Max1.SetActive(false);
+        }
+
+        if (Input.GetKeyUp(KeyCode.E)) 
+        {
+            if(_playerhp > 0) 
+            {
+                _playerhp--;
+            }
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            if (_playerhp < 3)
+            {
+                _playerhp++;
+            }
+            
+        }
+    }
+}
