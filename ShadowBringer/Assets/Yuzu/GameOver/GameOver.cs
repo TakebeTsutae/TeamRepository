66ecfb01f76475355a03a313ef35ad6426a868f1
MizukamiYuzu <fko2647069@stu.o-hara.ac.jp>
2026-06-11 00:09:58 +0900

    _GameOver

---
+++ ShadowBringer/Assets/Yuzu/GameOver/GameOver.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject click;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        click.SetActive(false);
+        // 自身のSpriteRendererを取得
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // 初期状態は非表示（透明）にする
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.enabled = false;
+        }
+    }
+
+    // マウスカーソルがコライダーに入ったとき
+    void OnMouseEnter()
+    {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.enabled = true; // 表示
+        }
+    }
+
+    // マウスカーソルがコライダーから出たとき
+    void OnMouseExit()
+    {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.enabled = false; // 非表示
+        }
+    }
+    void OnMouseDown()
+    {
+        
+        StartCoroutine(Click());
+
+    }
+
+    // クリック時の具体的な処理を書くための関数（自由に変更してください）
+    
+    private IEnumerator Click() 
+    {
+        Debug.Log(gameObject.name + " がクリックされました！");
+        click.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        click.SetActive(false);
+        if(this.gameObject.tag == "title")
+        {
+            //SceneManager.LoadScene("");   //タイトルのシーン名記入
+            Debug.Log("title");
+        }
+        else if (this.gameObject.tag == "retry") 
+        {
+            //SceneManager.LoadScene("");   //リトライのシーン名記入
+            Debug.Log("retry");
+        }
+    }
+
+
+}
+
