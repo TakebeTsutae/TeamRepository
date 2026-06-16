c68f689268740759aed5f5e19d86d4785297551c
hirayamamitsuha <fko2647022@stu.o-hara.ac.jp>
2026-06-16 18:49:40 +0900

    クリアコントローラーいじった

--- ShadowBringer/Assets/Mitsuha/ClearSceneController.cs
+++ ShadowBringer/Assets/Mitsuha/ClearSceneController.cs
@@ -4,8 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class ClearSceneController : MonoBehaviour
 {
+
+    // 遷移先のシーン名（Inspectorで設定）
     [SerializeField] private string nextSceneName = "TitleScene";
 
+    private float timer = 0f;
+
+    bool _moveBossScene = true;
     public void OnTitleButton()
     {
         SceneManager.LoadScene(nextSceneName);
