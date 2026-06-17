74e8cc9185f2815441ebf289aee78c892c30cccb
Reiji0115 <fko2647019@stu.o-hara.ac.jp>
2026-06-17 18:55:29 +0900

    GameOverシーンへの遷移、アニメーションに触れ始めた

---
+++ ShadowBringer/Assets/Reiji/playercontroller1.cs
@@ -0,0 +1,453 @@
+using JetBrains.Annotations;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using static UnityEditor.Progress;
+
+
+public class PlayerController1 : MonoBehaviour
+{
+    // アイテム所持判定のための変数
+
+    // PlayerController.cs の中にこれを追加
+    public string currentItem;
+
+    private GameObject _itemManager;
+
+    private const int kMaxHp= 3;
+
+    //  プレイヤーのHP
+    public int _playerHp = kMaxHp;
+
+    public int _attackTotal { get; private set;  } = 0;
+
+    // 攻撃力
+    public int _attack { get; private set; } = 0;
+
+    public int _attackWeapon = 3;
+
+    // 移動速度
+    public float moveSpeed = 7f;
+
+    // アクセサリーによる追加移動速度
+    private float _accessoriesMoveSpeed;
+
+    public int currentHP = 3;
+    public int maxHP = 3;
+
+
+    // ジャンプ力
+    Rigidbody2D rigid2D;
+    float jumpForce = 370f;
+
+    // 入力値
+    private float moveInput;
+    public bool isFacingRight = true;
+
+    // 地面にいるか
+    private bool isGrounded;
+
+    // リスト要素
+    private int _arrayElement;
+
+    // リスト1番目の情報取得用変数
+    private string _firstItem;
+
+    // タグの取得変数
+    public string[] _accessories = new string[3];
+
+    // 武器の所持変数
+    public string weapon;
+
+    // tagを取得したか(ItemPickupにてtrueとfalseをいじる)
+    public bool _Gettag = false;
+
+    private bool _item = false;
+
+    private bool _GetKey=false;
+
+    [Header("Jump Settings")]
+    // --- 追加: コヨーテタイムの設定 (秒) ---
+    [SerializeField] private float coyoteTime = 0.2f;
+    private float coyoteCounter;
+
+    // --- 追加: クールタイムの設定 (秒) ---
+    [SerializeField] private float jumpCooldown = 0.5f;
+    private float jumpCooldownCounter;
+
+    [Header("Dash Settings")]
+    [SerializeField] private float dashForce = 25f;
+    private bool isDashing = false;
+
+    // ダッシュのクールタイム用の箱(関数)
+    [SerializeField] private float dashCooldown = 1.0f;  // ダッシュの待ち時間(１秒)
+    private float dashCooldownCounter;                    // 残り時間を数えるタイマー
+
+    [Header("Damage Settings")]
+    [SerializeField] private float invincibleTime = 1.0f;
+    private float invincibleCounter;
+
+    [SerializeField] private string gameover;
+
+    // -----------------------------------------------------------------------
+    void Start()
+    {
+        _accessoriesMoveSpeed = 0f;
+        Application.targetFrameRate = 60;
+        rigid2D = GetComponent<Rigidbody2D>();
+        _itemManager = GameObject.Find("ItemManager");
+        _arrayElement = 0;
+        _Gettag = false;
+        weapon = "Ken";
+
+
+
+    }
+
+    void Update()
+    {
+     //   Debug.Log(maxHP);
+
+
+        _attackTotal = _attackWeapon + _attack;
+        
+
+        if (Keyboard.current.eKey.isPressed)
+        {
+            _GetKey = true;
+        }
+        else
+        {
+            _GetKey = false;
+        }
+
+
+
+        if (_Gettag == true)
+        {
+            // 配列の範囲内に収まるように安全弁をつける
+            if (_arrayElement > 0 && _arrayElement <= _accessories.Length)
+            {
+                _accessories[_arrayElement - 1] = currentItem;
+                Status(_accessories[_arrayElement - 1]);
+            }
+
+            if (_arrayElement >= 3)
+            {
+                List(_accessories[0]);
+                _arrayElement = 2; // 古いものを捨てたので、現在の所持数を2に補正する
+            }
+
+            _Gettag = false;
+        }
+        
+        // A,Dキー / ←→キー
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        {
+            moveInput = -1f-_accessoriesMoveSpeed;
+            isFacingRight = false;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+        {
+            moveInput = 1f+ _accessoriesMoveSpeed;
+            isFacingRight = true;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            moveInput = 0f;
+        }
+
+        
+
+        // --- タイマーの更新処理 ---
+        // 1. コヨーテタイムの計算
+        if (isGrounded)
+        {
+            coyoteCounter = coyoteTime; // 地面にいる間は常に満タン(0.2秒)
+        }
+        else
+        {
+            coyoteCounter -= Time.deltaTime; // 地面から離れたらカウントダウン
+        }
+
+        // 2. ジャンプのクールタイムの計算
+        if (jumpCooldownCounter > 0)
+        {
+            jumpCooldownCounter -= Time.deltaTime; // クールタイムを減らす
+        }
+
+        // --- ジャンプ処理 ---
+        // 条件：スペースが押された ＆ コヨーテタイム内 ＆ クールタイムが終わっている
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && coyoteCounter > 0f && jumpCooldownCounter <= 0f)
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+
+            // ジャンプ成功時の処理
+            coyoteCounter = 0f; // 空中での連続ジャンプを防ぐため、猶予をゼロにする
+            jumpCooldownCounter = jumpCooldown; // クールタイム(0.5秒)をセット
+        }
+        if (Mouse.current.rightButton.wasPressedThisFrame&&dashCooldownCounter<=0f)
+        {
+            dashCooldownCounter = dashCooldown;
+            StartCoroutine(DashRoutine());
+        }
+
+        // ダッシュのクールタイムの計算
+        if (dashCooldownCounter > 0)
+        {
+            dashCooldownCounter -= Time.deltaTime; // タイマーが0になるまで毎フレーム減速する
+        }
+
+        if (isDashing) return;
+
+        if(invincibleCounter>0)
+        {
+            invincibleCounter -= Time.deltaTime;
+        }
+
+        if (isDashing) return;
+
+        if(isDead())
+        {
+            //_playerHp = kMaxHp;
+            SceneManager.LoadScene(gameover);
+        }
+
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        if (invincibleCounter > 0f)
+        {
+            return;
+        }
+
+        // ダメージを適用
+        _playerHp -= damageAmount;
+        Debug.Log($"ダメージを{damageAmount}受けた！残りHP:{_playerHp}");
+
+        // 無敵タイマーをセット(満タンにする)
+        invincibleCounter = invincibleTime;
+    }
+
+    // ダッシュ
+    /*
+      public void OnDash(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+
+        // ↓ダッシュのアニメーションを追加するためのコード
+       // Animator.SetTrigger(dashParamHash);
+        StartCoroutine(DashRoutine());
+    }
+    */
+
+    // ダッシュ用コルーチン
+    private IEnumerator DashRoutine()
+    {
+        isDashing = true;
+
+        rigid2D.linearVelocity = new Vector2(0, rigid2D.linearVelocity.y);
+
+        float i = 0;
+        float deltaTime = 0;
+        while (deltaTime < 0.2f)
+        {
+            if (isFacingRight) rigid2D.MovePosition(rigid2D.position += new Vector2((dashForce - i) * Time.deltaTime, 0));  // 右を向いてるとき
+            else rigid2D.MovePosition(rigid2D.position -= new Vector2((dashForce - i) * Time.deltaTime, 0));             // 左を向いてるとき
+            i += Time.deltaTime * 20f;
+            deltaTime += Time.deltaTime;
+            yield return null;
+        }
+        isDashing = false;
+    }
+
+
+
+    void FixedUpdate()
+    {
+        if (isDashing) return;
+        // 左右移動
+        rigid2D.linearVelocity = new Vector2(moveInput * moveSpeed, rigid2D.linearVelocity.y);
+
+        if (Mouse.current.rightButton.wasPressedThisFrame && dashCooldownCounter <= 0f)
+        {
+            dashCooldownCounter = dashCooldown; // タイマーを1秒満タンにセットする
+            StartCoroutine(DashRoutine());
+        }
+    }
+
+    // tagにぶつかったときの処理たち
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        if(collision.gameObject.CompareTag("hari"))
+        {
+            isGrounded = true;
+            TakeDamage(1);
+        }
+        if(collision.gameObject.CompareTag("enemy"))
+        {
+            TakeDamage(1);
+            Debug.Log("敵にぶつかった！");
+            Debug.Log(_playerHp);
+        }
+        
+        
+    }
+
+    // 接触した瞬間の処理（敵の攻撃など、キー入力に関係ないもの）
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("enemyAttack"))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    // 【追加】トリガーに触れている間、毎フレーム呼び出される処理
+    // 【修正版】トリガーに触れている間、毎フレーム呼び出される処理
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        // すでにアイテム取得処理が始まっている（_GettagがTrue）なら、このフレームは何もせず返す
+        if (_Gettag) return;
+
+        // アイテムのタグを持っている場合のみ、Eキーの入力をチェックする
+        if (collision.CompareTag("Up") || collision.CompareTag("Speed") || collision.CompareTag("Tue") || collision.CompareTag("Ken") || collision.CompareTag("HP"))
+        {
+            // 触れている間にEキーが押されたら取得
+            if (Keyboard.current.eKey.isPressed)
+            {
+                
+
+                if (collision.CompareTag("Up"))
+                {
+                    _arrayElement++;
+                    currentItem = "Up";
+                }
+                else if (collision.CompareTag("Speed"))
+                {
+                    _arrayElement++;
+                    currentItem = "Speed";
+                }
+                else if (collision.CompareTag("Ken"))
+                {
+                    weapon = "Ken";
+                    _attackWeapon = 3;
+                }
+                else if (collision.CompareTag("Tue"))
+                {
+
+                    weapon = "Tue";
+                    _attackWeapon = 2;
+                }
+                else if (collision.CompareTag("HP"))
+                {
+                    _arrayElement++;
+                    weapon = "HP";
+                    currentHP += 20;
+                }
+
+                //  Debug.Log($"currentItemを取得しました: {currentItem}");
+
+                // 2重判定を防ぐために即座にコライダーを無効化する
+                collision.enabled = false;
+
+
+                _Gettag = true;
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+
+
+    // 地面から離れた
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    // アクセサリーによるステータス処理
+    private void Status(string accessories)// アイテムをゲットした
+    {
+
+        if (accessories == "Up")
+        {
+            _attack += 2;
+
+        }
+        else if (accessories == "Speed")
+        {
+            _accessoriesMoveSpeed += 1.2f;
+            
+        }
+        else if (accessories == "HP")
+        {
+            maxHP += 1;     // 最大HPを増加
+            currentHP += 1; // 今のHPも一緒に増やす（回復も兼ねる）
+
+            Debug.Log("最大HP増加:" + currentHP + " / " + maxHP);
+        }
+        else
+        {
+            return;
+        }
+
+
+
+    }
+    private void List(string accessories)　// アイテムを失い効果が消える
+    {
+
+
+        if (accessories == "Up")
+        {
+
+            _attack -= 2;
+
+        }
+        else if (accessories == "Speed")
+        {
+
+            _accessoriesMoveSpeed -= 1.2f;
+
+        }
+        else if (accessories == "HP")
+        {
+            maxHP -= 1;
+
+            // HPがmaxを超えないように調整
+            if (currentHP > maxHP)
+            {
+                currentHP = maxHP;
+            }
+        }
+        else
+        {
+            return;
+        }
+        
+        _accessories[0] = _accessories[1];
+        _accessories[1] = _accessories[2];
+    }
+
+    public int GetHP()
+    {
+        return _playerHp;
+    }
+    public bool isDead()
+    {
+        return _playerHp <= 0;
+    }
+
+}
