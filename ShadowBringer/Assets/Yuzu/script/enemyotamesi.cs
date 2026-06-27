c100d64b7bfb799c11b4daeb41ccbc3c3319e81e
MizukamiYuzu <fko2647069@stu.o-hara.ac.jp>
2026-06-28 03:43:13 +0900

    enemy

--- ShadowBringer/Assets/Yuzu/script/enemyotamesi.cs
+++ ShadowBringer/Assets/Yuzu/script/enemyotamesi.cs
@@ -5,5 +5,17 @@ using UnityEngine;
 
 public class enemyotamesi : MonoBehaviour
 {
+    Vector2 pos;
+    private void Start()
+    {
+    }
 
+    void FixedUpdate()
+    {
+        Rigidbody2D enemyRb = this.GetComponent<Rigidbody2D>(); // enemy‚Ìrigidbodyæ“¾
+        Vector2 enemyForce = new Vector2(5.0f, 0.0f); // —Í‚Ìİ’è
+        enemyRb.AddForce(enemyForce);   // w’è‚µ‚½—Í‚ğ—^‚¦‚é©w’è‚³‚ê‚½—Í
+    }
+    
+    
 }
