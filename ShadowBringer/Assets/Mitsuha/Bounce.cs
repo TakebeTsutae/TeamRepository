401f58f587d59f0259c589f9a63761efa6da416e
hirayamamitsuha <fko2647022@stu.o-hara.ac.jp>
2026-07-03 11:59:19 +0900

    なんかした

--- ShadowBringer/Assets/Mitsuha/Bounce.cs
+++ ShadowBringer/Assets/Mitsuha/Bounce.cs
@@ -1,11 +1,11 @@
-using Unity.VisualScripting;
+﻿using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rb;
     //ジャンプの高さ調整
-    private float bounce = 20.0f;
+    private float bounce = 5.0f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("bounce"))
