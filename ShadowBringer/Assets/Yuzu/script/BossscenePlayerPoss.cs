d1cc8a7c88a4b159640f716b7c78ffb438d4423a
Reiji0115 <fko2647019@stu.o-hara.ac.jp>
2026-07-13 17:30:59 +0900

    bosssceneにscript書いた

---
+++ ShadowBringer/Assets/Yuzu/script/BossscenePlayerPoss.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BossscenePlayerPoss : MonoBehaviour
+{
+    private GameObject player;
+
+    private Vector2 InitPoss = new(-19, -3);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.Find("player");
+
+        player.transform.position= InitPoss;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
