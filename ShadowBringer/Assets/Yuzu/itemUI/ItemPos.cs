162cd4d94ca8e4431f565b91604e295dde9f2a09
TakebeTsutae <fko2647060@stu.o-hara.ac.jp>
2026-07-14 18:48:45 +0900

    メイン合わせ終了

---
+++ ShadowBringer/Assets/Yuzu/itemUI/ItemPos.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ItemPos : MonoBehaviour
+{
+    private GameObject playerObj;
+    private Vector2 playerPos;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerObj = GameObject.Find("enemySuraimu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = playerObj.transform.position;
+        this.transform.position = new Vector2 (playerPos.x, playerPos.y+0.3f);
+    }
+}
