5798714163f96b58171d814e138aa92532a65135
MizukamiYuzu <fko2647069@stu.o-hara.ac.jp>
2026-07-15 18:53:05 +0900

    aaaa

---
+++ ShadowBringer/Assets/Yuzu/script/HpUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HpUI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        MeshRendere meshRenderer = GetComponent<MeshRenderer>();
+
+        Mesh mesh = new Mesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
